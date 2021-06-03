3b67893cc4b2a61e6d15aafb0ba4218209a5d35b
Khoa <khoa150899@gmail.com>
2021-06-03 02:09:36 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,20 @@
+package org.cop3330;
+
+import java.util.Scanner;
+import java.util.Calendar; //to get time & date from system
+
+//program to get age and retirement age from user to calculate amount of time before retirement and retirement year
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        int year = Calendar.getInstance().get(Calendar.YEAR); //gets current year from system
+        System.out.print("What is your current age? ");
+        int age = (int) input.nextInt();
+        System.out.print("At what age would you like to retire? ");
+        int ageRetire = (int) input.nextInt();
+        System.out.println("You have " + retire.diff(age, ageRetire) + " years left before you can retire.");
+        System.out.println("Since it's currently " + year + ", you can retire in " + retire.year(year, retire.diff(age, ageRetire)));
+    }
+}
---
+++ src/main/java/org/cop3330/retire.java
@@ -0,0 +1,11 @@
+package org.cop3330;
+
+public class retire {
+    public static int diff(int age, int ageRetire) {
+        return ageRetire - age;
+    }
+
+    public static int year(int year, int diff) {
+        return year + diff;
+    }
+}
